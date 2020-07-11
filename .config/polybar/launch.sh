b31f20123855e4a4bee5b114ee92b57d9d1c40b0
Andreas Bjoru <andreas.bjoru@gmail.com>
2020-07-10 23:33:35 -0400

    attempt to launch polybar for single monitor systems

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,9 +10,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
   #WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-xmonad &
 #done
 
-polybar --reload m1 &
-polybar --reload m2 &
-polybar --reload m3 &
-polybar --reload m4 &
+# Default to standard monitor unless multiple
+monitors=( $(polybar --list-monitors) )
+if [ ${#monitors[@]} -eq 1 ]; then
+  MONITOR=${monitors[0]} polybar --reload m1 &
+else
+  polybar --reload m1 &
+  polybar --reload m2 &
+  polybar --reload m3 &
+  polybar --reload m4 &
+fi
 
 echo "Bars launched..."
