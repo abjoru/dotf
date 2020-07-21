d1169284c7df84aad0946c572a675c63c7f1d556
Andreas Bjoru <andreas.bjoru@gmail.com>
2020-07-21 14:57:01 -0400

    cleaned up base config

--- .config/xmonad/scripts/autostart.sh
+++ .config/xmonad/scripts/autostart.sh
@@ -8,27 +8,8 @@ function run {
   fi
 }
 
-# cursor active at boot
-#xsetroot -cursor_name left_ptr &
-
-# starting user applications at boot
-#nitrogen --restore &
-
 # start conky for shortcut hints
-#(conky -c $HOME/.config/xmonad/scripts/system-overview) &
+(conky -c $HOME/.config/xmonad/scripts/system-overview) &
 
 # starting utility applications at boot 
 run picom --config $HOME/.config/xmonad/scripts/picom.conf
-
-# Polybar
-#source $HOME/.config/polybar/launch.sh
-
-# Trayer
-#run trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x292d3e --height 22
-
-# sys-tray apps
-sleep 1
-run streamdeck
-#run megasync
-#run volumeicon
-#run synology-drive
