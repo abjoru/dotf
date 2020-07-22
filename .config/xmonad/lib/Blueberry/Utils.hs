f05ba048da462821146ef6a53f8808e7384558f8
Andreas Bjoru <andreas.bjoru@gmail.com>
2020-07-22 17:16:15 -0400

    Generate game menu from filesystem

--- .config/xmonad/lib/Blueberry/Utils.hs
+++ .config/xmonad/lib/Blueberry/Utils.hs
@@ -1,5 +1,5 @@
 module Blueberry.Utils (
-  sanitizeName, getGames
+  sanitizeName, listGames
 ) where
 
 import Control.Applicative
@@ -8,20 +8,46 @@ import Control.Monad
 import System.Directory
 import System.FilePath
 
+import Data.Tree
+import Data.List (find)
+import Data.Maybe (catMaybes)
+
+import XMonad
+import qualified XMonad.Actions.TreeSelect as TS
+
 -- Sanitize a name such that any characters like '-_"' have 
 -- either been removed or replaced with space.
-sanitizeName :: (Show a) => a -> String
+sanitizeName :: String -> String
 sanitizeName ('-':xs)   = ' ' : sanitizeName xs
 sanitizeName ('_':xs)   = ' ' : sanitizeName xs
 sanitizeName ('"':xs)   = sanitizeName xs
+sanitizeName (x:xs)     = x : sanitizeName xs
 sanitizeName ""         = ""
 
--- Filter for *.sh files
-shellFileFilter :: FilePath -> Bool
-shellFileFilter f = takeExtension f == ".sh"
+-- Attempt to extract game launcher script from contents
+maybeLauncher :: [FilePath] -> Maybe FilePath
+maybeLauncher = find (\a -> ".sh" == takeExtension a)
 
-getGames :: IO [(String, FilePath, String)]
-getGames = mapM processGame <$> listDirectory "/home/abjoru/Games"
+-- Combine game dir with launcher file if any
+mkEntry :: FilePath -> Maybe FilePath -> Maybe (String, FilePath)
+mkEntry a (Just b)  = Just (sanitizeName $ takeBaseName a, b)
+mkEntry a Nothing = Nothing
 
-processGame :: FilePath -> IO [Maybe (String, FilePath, String)]
-processGame p = mapN (\a -> (p, a)) <$> filter shellFileFilter <$> listDirectory p
+-- Convert game folder into a pair (name, launcher)
+processGame :: FilePath -> IO (Maybe (String, FilePath))
+processGame p = do
+  contents <- listAbsDir p
+  let a = maybeLauncher contents
+      b = mkEntry p a
+  return b
+
+-- List games in the given directory
+listGames :: FilePath -> IO [(String, FilePath)]
+listGames p = do
+  files <- listAbsDir p
+  maybeGames <- sequence $ map processGame files
+  return $ catMaybes maybeGames
+
+-- List directory contents as absolute paths
+listAbsDir :: FilePath -> IO [FilePath]
+listAbsDir d = listDirectory d >>= mapM (canonicalizePath . (d </>))
