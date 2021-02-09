a149a4b138d3d472e4f755195a00959af0e5d8c8
Andreas Bjoru <andreas.bjoru@gmail.com>
2021-02-09 01:38:39 -0500

    bootstrap packer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,18 @@
 local cmd = vim.cmd
 local dotf = require('dotf')
+local exec = vim.api.nvim_command
+local fn = vim.fn
+
+--------------------
+-- Install Packer --
+--------------------
+
+local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  exec('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  exec('packadd packer.nvim')
+end
 
 dotf.before()
 
