7128955753b2812ca6e8da3a2863cb9ee5548e6d
Andreas Bjoru <andreas.bjoru@gmail.com>
2021-02-16 19:04:11 -0500

    update packages for osx

--- .config/dotf/scripts/shell/postinstall.sh
+++ .config/dotf/scripts/shell/postinstall.sh
@@ -6,13 +6,20 @@
 # Write completion script for DotF
 # TODO base install might not have xdg bin dir in path!
 # default to ~/.local/bin/dotf if which fails
+if [ -f "$CONFIG/zsh/functions/_dotf" ]; then
+  rm "$CONFIG/zsh/functions/_dotf"
+fi
+
 if which dotf; then
   dotf --zsh-completion-script `which dotf` >> $CONFIG/zsh/functions/_dotf
-else
+elif [ -f "$HOME/.local/bin/dotf" ]; then
   dotf --zsh-completion-script $HOME/.local/bin/dotf >> $CONFIG/zsh/functions/_dotf
+else
+  echo "DotF not found! Cannot create completions..."
 fi
 
 # Update shell to ZSH
+# TODO Check shell and only change if not zsh!
 if is_linux; then
   chsh -s /usr/bin/zsh
 fi
